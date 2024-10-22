398a339eaa8999be031693b29780a00c71f94122
Raphael G <160821025+raphaelgoldwaser@users.noreply.github.com>
2024-10-23 10:40:26 +1100

    fix git

--- lib/codebuild-stack.ts
+++ lib/codebuild-stack.ts
@@ -1,7 +1,6 @@
 // lib/codebuild-stack.ts
 import * as cdk from 'aws-cdk-lib';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
-import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
@@ -9,13 +8,13 @@ export class CodeBuildStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create a CodeCommit repository
-    const repo = new codecommit.Repository(this, 'CDKRepo', {
-      repositoryName: 'my-cdk-repo',
-      description: 'Repository for CDK code',
-    });
-
     const project = new codebuild.Project(this, 'CDKDeployProject', {
+      projectName: 'CDKDeployProject',
+      source: codebuild.Source.gitHub({
+        owner: 'raph-foxtel',
+        repo: 'livestreaming-test-infrastructure',
+        webhook: false, // This sets up a webhook to trigger builds on push
+      }),
       buildSpec: codebuild.BuildSpec.fromObject({
         version: '0.2',
         phases: {
@@ -27,7 +26,8 @@ export class CodeBuildStack extends cdk.Stack {
           },
           build: {
             commands: [
-              'cdk deploy --context stack-name=$STACK_NAME --require-approval never',
+              'npm run build',
+              'cdk deploy --all --require-approval never',
             ],
           },
         },
@@ -35,10 +35,8 @@ export class CodeBuildStack extends cdk.Stack {
       environment: {
         buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
       },
-      source: codebuild.Source.codeCommit({ repository: repo }),
     });
 
-    // Grant permissions to deploy CloudFormation stacks
     project.addToRolePolicy(new iam.PolicyStatement({
       actions: [
         'cloudformation:*',
@@ -48,16 +46,13 @@ export class CodeBuildStack extends cdk.Stack {
         'ssm:*',
         'logs:*',
         'lambda:*',
-        // Add any other necessary permissions based on the resources your stack is creating
       ],
       resources: ['*'],
     }));
-    
 
-    // Output the CodeCommit repository URL
-    new cdk.CfnOutput(this, 'RepositoryCloneUrlHttp', {
-      value: repo.repositoryCloneUrlHttp,
-      description: 'CodeCommit Repository Clone URL (HTTPS)',
+    new cdk.CfnOutput(this, 'CodeBuildProjectName', {
+      value: project.projectName,
+      description: 'CodeBuild Project Name',
     });
   }
 }
--- lib/lambda/ci-cd-start-stack/index.py
+++ lib/lambda/ci-cd-start-stack/index.py
@@ -5,25 +5,20 @@ import os
 
 codebuild = boto3.client('codebuild')
 
-def handler(event, context):
+def lambda_handler(event, context):
     try:
-        body = json.loads(event['body'])
-        stack_name = body['stackName']
-
+        project_name = os.environ['CODEBUILD_PROJECT_NAME']
+        
         response = codebuild.start_build(
-            projectName=os.environ['CODEBUILD_PROJECT_NAME'],
-            environmentVariablesOverride=[
-                {
-                    'name': 'STACK_NAME',
-                    'value': stack_name,
-                    'type': 'PLAINTEXT'
-                }
-            ]
+            projectName=project_name
         )
 
         return {
             'statusCode': 200,
-            'body': json.dumps({'message': f'Deployment of stack {stack_name} started'})
+            'body': json.dumps({
+                'message': f'Deployment started for project {project_name}',
+                'buildId': response['build']['id']
+            })
         }
     except Exception as e:
         print(f"Error: {str(e)}")
